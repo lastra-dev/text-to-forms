b1c904aa138f585c1801ecef79b96e4a2ce99580
OscarGL <56526315+oscaragl13@users.noreply.github.com>
2020-12-07 23:06:57 -0600

    Add sample questions

--- main.py
+++ main.py
@@ -21,16 +21,16 @@ def main_loop():
     for i in range(0, len(lines), 5):
         add_question()
         send_info("//textarea[@class='appsMaterialWizTextinputTextareaInput exportTextarea']", -1, lines[i])
-        send_info("//input[@class='quantumWizTextinputSimpleinputInput exportInput']", -2, lines[i + 1])
-        send_info("//input[@class='quantumWizTextinputSimpleinputInput exportInput']", -1, lines[i + 2])
-        send_info("//input[@class='quantumWizTextinputSimpleinputInput exportInput']", -1, lines[i + 3])
-        send_info("//input[@class='quantumWizTextinputSimpleinputInput exportInput']", -1, lines[i + 4])
+        send_info("//input[@class='quantumWizTextinputSimpleinputInput exportInput']", -2, lines[i+1])
+        send_info("//input[@class='quantumWizTextinputSimpleinputInput exportInput']", -1, lines[i+2])
+        send_info("//input[@class='quantumWizTextinputSimpleinputInput exportInput']", -1, lines[i+3])
+        send_info("//input[@class='quantumWizTextinputSimpleinputInput exportInput']", -1, lines[i+4])
 
 
 lines = []
 with open("questions.txt", "r", encoding="utf8") as file:
     for line in file:
         if '\n' in line:
-            lines.append(line[:-2])
+            lines.append(line[:-1])  # avoids enter
         else:
             lines.append(line)
