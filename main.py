83dccbedfcfd465eae3921f8f018ab0f83bd0fb8
OscarGL <56526315+oscaragl13@users.noreply.github.com>
2020-12-09 16:01:26 -0600

    Add instructions Modify documentation Add exceptions Add instructions

--- main.py
+++ main.py
@@ -1,47 +1,60 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException, NoSuchElementException
 import xpath
 import time
-
-driver = webdriver.Chrome()
 
 
 def add_question():
     """Locates and clicks on add question button in browser"""
 
-    add_question_button = driver.find_element_by_xpath(xpath.ADD_QUESTION_BUTTON)
-    add_question_button.click()
+    try:
+        add_question_button = driver.find_element_by_xpath(xpath.ADD_QUESTION_BUTTON)
+    except NoSuchElementException:
+        input("ERROR: Make sure to be on your Google form and do not interfere with the browser!\n"
+              "Press Enter to exit...")
+        driver.close()
+        exit(0)
+    else:
+        add_question_button.click()
 
 
 def send_info(location, element, info):
     """Locates element in browser and sends information
 
-    Gets a list of elements passed by an xpath location in the browser.
-    It then sends information to the n-element of that list.
-    If the element is the first answer option in google forms it gets
-    clicked in order to get cleared, this is because
-    elements[element].clear() is not working here.
+    Gets element from list of elements and send information to that element
+
+    Parameters
+    ----------
+    location : str
+        Xpath location of the elements
+    element : int
+        Index of element in the location
+    info : str
+        Information to send to the element in the location
     """
 
     elements = driver.find_elements_by_xpath(location)
     # if first answer
     if element == -2:
-        # click needed to clear answer box
+        # click needed to clear answer box, .clear() not working...
         elements[element].click()
     elements[element].send_keys(info)
     # necessary time for google forms to finish entering the text
     time.sleep(0.5)
 
 
-def fill_list():
+def fill_list() -> list:
     """Fills list in which questions are taken to fill Google form"""
 
+    questions_list = []
     with open("questions.txt", "r", encoding="utf8") as file:
         for line in file:
             if '\n' in line:
                 # avoids return
-                questions.append(line[:-1])
+                questions_list.append(line[:-1])
             else:
-                questions.append(line)
+                questions_list.append(line)
+    return questions_list
 
 
 def main_loop():
@@ -72,16 +85,30 @@ def main_loop():
 ########################################################################
 
 
-print("--------------------------------------------\n"
-      "            SELENIUM EXAM FILLER\n"
-      "--------------------------------------------\n"
-      "More info: https://github.com/oscaragl13/selenium-exam-filler\n")
+if __name__ == "__main__":
+    print("--------------------------------------------\n"
+          "            SELENIUM EXAM FILLER\n"
+          "--------------------------------------------\n"
+          "Instructions:\n"
+          "1. Go to forms.google.com\n"
+          "2. Login to your Google account\n"
+          "   allow less secure apps if having problems:\n"
+          "   https://myaccount.google.com/lesssecureapps\n"
+          "3. Add a new form\n"
+          "4. Hit Enter\n"
+          "\nMore info: https://github.com/oscaragl13/selenium-exam-filler\n")
 
-
-questions = []
-fill_list()
-
-print(str(questions.count('') + 1) + " questions to add.")
-input("Hit Enter when you get to your Google form...\n")
-main_loop()
-input("Process finished, press Enter to exit...")
+    try:
+        driver = webdriver.Chrome()
+        questions = fill_list()
+    except WebDriverException:
+        input("ERROR: Make sure to have chromedriver.exe inside the root of the project, Chrome installed, "
+              "and both programs up to date.\npress Enter to exit...")
+    except FileNotFoundError:
+        input("ERROR: Make sure to have questions.txt inside the root of the project and add questions to it.\n"
+              "press Enter to exit...")
+    else:
+        print(str(questions.count('') + 1) + " questions to add.")
+        input("Hit Enter when you get to your Google form...\n")
+        main_loop()
+        input("Process finished, press Enter to exit...")
